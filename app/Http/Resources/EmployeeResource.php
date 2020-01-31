0ef0de07d34760ad62a27c8a50a175fa8bcac614
Angel <pixan-angel@pixan.io>
2020-01-31 13:22:59 -0600

    fix

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -18,6 +18,7 @@ class EmployeeResource extends JsonResource
             'id' => $this->id,
             'code' => $this->code,
             'name' => $this->name,
+            'email' => $this->email,
             'salary_mxn' => $this->salary_mxn,
             'salary_usd' => $this->salary_usd,
             'address' => $this->address,
